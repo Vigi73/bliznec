3b5cfcaee9b88eff584d2e817459bacc76329c6c
Vigi73 <vigi73@mail.ru>
2021-12-28 19:13:50 +0700

    edit case

--- clFish/Form1.cs
+++ clFish/Form1.cs
@@ -793,131 +793,39 @@ namespace clFish
                 {
                     switch (list[i])
                     {
-                        case 0:
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
-                        case 5:
-                        case 6:
-                        case 7:
-                        case 8:
-                        case 9:
-                            od.Add(list[i]); // Добавляем в список если до 10
-                                             //listBox6.Items.Add(list[i]);
+                        case < 10:
+                            od.Add(list[i]); // Добавляем в список если до 10                                            
                             break;
 
-                        case 10:
-                        case 11:
-                        case 12:
-                        case 13:
-                        case 14:
-                        case 15:
-                        case 16:
-                        case 17:
-                        case 18:
-                        case 19:
+                        case < 20:
                             dv.Add(list[i]); // Добавляем в список если до 20
                             break;
 
-                        case 20:
-                        case 21:
-                        case 22:
-                        case 23:
-                        case 24:
-                        case 25:
-                        case 26:
-                        case 27:
-                        case 28:
-                        case 29:
+                        case < 30:
                             tr.Add(list[i]); // Добавляем в список если до 30
                             break;
 
-                        case 30:
-                        case 31:
-                        case 32:
-                        case 33:
-                        case 34:
-                        case 35:
-                        case 36:
-                        case 37:
-                        case 38:
-                        case 39:
+                        case < 40:
                             ch.Add(list[i]); // Добавляем в список если до 40
                             break;
 
-                        case 40:
-                        case 41:
-                        case 42:
-                        case 43:
-                        case 44:
-                        case 45:
-                        case 46:
-                        case 47:
-                        case 48:
-                        case 49:
+                        case < 50:
                             py.Add(list[i]); // Добавляем в список если до 50
                             break;
 
-                        case 50:
-                        case 51:
-                        case 52:
-                        case 53:
-                        case 54:
-                        case 55:
-                        case 56:
-                        case 57:
-                        case 58:
-                        case 59:
+                        case < 60:
                             sh.Add(list[i]); // Добавляем в список если до 60
                             break;
 
-                        case 60:
-                        case 61:
-                        case 62:
-                        case 63:
-                        case 64:
-                        case 65:
-                        case 66:
-                        case 67:
-                        case 68:
-                        case 69:
+                        case < 70:
                             se.Add(list[i]); // Добавляем в список если до 70
                             break;
 
-                        case 70:
-                        case 71:
-                        case 72:
-                        case 73:
-                        case 74:
-                        case 75:
-                        case 76:
-                        case 77:
-                        case 78:
-                        case 79:
+                        case < 80:
                             vo.Add(list[i]); // Добавляем в список если до 80
                             break;
 
-                        case 80:
-                        case 81:
-                        case 82:
-                        case 83:
-                        case 84:
-                        case 85:
-                        case 86:
-                        case 87:
-                        case 88:
-                        case 89:
-                        case 90:
-                        case 91:
-                        case 92:
-                        case 93:
-                        case 94:
-                        case 95:
-                        case 96:
-                        case 97:
-                        case 98:
-                        case 99:
+                        case < 100:
                             de.Add(list[i]); // Добавляем в список если до 70
                             break;
                     }
@@ -1056,131 +964,39 @@ namespace clFish
                 {
                     switch (list[i])
                     {
-                        case 0:
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
-                        case 5:
-                        case 6:
-                        case 7:
-                        case 8:
-                        case 9:
-                            od.Add(list[i]); // Добавляем в список если до 10
-                                             //listBox6.Items.Add(list[i]);
+                        case <10:
+                            od.Add(list[i]); // Добавляем в список если до 10                                            
                             break;
 
-                        case 10:
-                        case 11:
-                        case 12:
-                        case 13:
-                        case 14:
-                        case 15:
-                        case 16:
-                        case 17:
-                        case 18:
-                        case 19:
+                        case <20:
                             dv.Add(list[i]); // Добавляем в список если до 20
                             break;
 
-                        case 20:
-                        case 21:
-                        case 22:
-                        case 23:
-                        case 24:
-                        case 25:
-                        case 26:
-                        case 27:
-                        case 28:
-                        case 29:
+                        case <30:
                             tr.Add(list[i]); // Добавляем в список если до 30
                             break;
 
-                        case 30:
-                        case 31:
-                        case 32:
-                        case 33:
-                        case 34:
-                        case 35:
-                        case 36:
-                        case 37:
-                        case 38:
-                        case 39:
+                        case <40:
                             ch.Add(list[i]); // Добавляем в список если до 40
                             break;
 
-                        case 40:
-                        case 41:
-                        case 42:
-                        case 43:
-                        case 44:
-                        case 45:
-                        case 46:
-                        case 47:
-                        case 48:
-                        case 49:
+                        case <50:
                             py.Add(list[i]); // Добавляем в список если до 50
                             break;
 
-                        case 50:
-                        case 51:
-                        case 52:
-                        case 53:
-                        case 54:
-                        case 55:
-                        case 56:
-                        case 57:
-                        case 58:
-                        case 59:
+                        case <60:
                             sh.Add(list[i]); // Добавляем в список если до 60
                             break;
 
-                        case 60:
-                        case 61:
-                        case 62:
-                        case 63:
-                        case 64:
-                        case 65:
-                        case 66:
-                        case 67:
-                        case 68:
-                        case 69:
+                        case <70:
                             se.Add(list[i]); // Добавляем в список если до 70
                             break;
 
-                        case 70:
-                        case 71:
-                        case 72:
-                        case 73:
-                        case 74:
-                        case 75:
-                        case 76:
-                        case 77:
-                        case 78:
-                        case 79:
+                        case <80:
                             vo.Add(list[i]); // Добавляем в список если до 80
                             break;
 
-                        case 80:
-                        case 81:
-                        case 82:
-                        case 83:
-                        case 84:
-                        case 85:
-                        case 86:
-                        case 87:
-                        case 88:
-                        case 89:
-                        case 90:
-                        case 91:
-                        case 92:
-                        case 93:
-                        case 94:
-                        case 95:
-                        case 96:
-                        case 97:
-                        case 98:
-                        case 99:
+                        case <100:
                             de.Add(list[i]); // Добавляем в список если до 70
                             break;
                     }
@@ -1199,6 +1015,16 @@ namespace clFish
             catch { }
 
             
+        }
+        // Начинаем играть профи
+        private void tmProf_Tick(object sender, EventArgs e)
+        {
+            if (chRange.Checked && listBox1.Items.Count > 0)
+            {
+                var lastRowForFox = listBox1.Items[^1].ToString();
+
+                var dataLogFish = lastRowForFox.Split(";");
+            }
         }
 
 
